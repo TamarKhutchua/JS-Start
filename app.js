eb091e679cec1a7c992bf37b43a3426895ab6928
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-08-30 21:08:54 +0400

    create functions

--- app.js
+++ app.js
@@ -15,3 +15,19 @@ console.log("jsMethod: " + typeof jsMethod);
 console.log("jsSymbol: " + typeof jsSymbol);
 console.log("emptyVariable: " + typeof emptyVariable);
 console.log("unusedVariable: " + typeof unusedVariable);
+
+console.log("8 x 8: " + square(8));
+console.log("8 + 20: " + add(8, 20));
+console.log("8 + 10: " + add(8));
+console.log("(8 x 8) + (8 + 10): " + squareAdd(8));
+
+function square(arg) {
+  return arg * arg;
+}
+function add(argOne, argTwo = 10) {
+  return argOne + argTwo;
+}
+function squareAdd(arg) {
+  let result = square(arg);
+  return result + add(arg);
+}
